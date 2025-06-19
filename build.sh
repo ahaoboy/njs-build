d4d499cd9d0e3213692cf9944ee663a57f6c767d
ahaoboy <504595380@qq.com>
2025-06-19 17:35:02 +0800

    qjs

--- build.sh
+++ build.sh
@@ -11,32 +11,33 @@ git clone https://github.com/nginx/njs.git njs --depth=1
 cd njs
 
 # njs-ng
-git clone https://github.com/quickjs-ng/quickjs quickjs-ng
-git checkout v0.9.0
-cd quickjs-ng
-cmake -B build
-cmake --build build --target njs
+# git clone https://github.com/quickjs-ng/quickjs quickjs-ng
+# cd quickjs-ng
+# git checkout v0.9.0
+# cmake -B build
+# cmake --build build --target qjs
 
-./configure \
-          --with-quickjs \
-          --cc-opt="$CC_OPT -Iquickjs-ng" \
-          --ld-opt="$LD_OPT -Lquickjs-ng/build" \
-|| cat build/autoconf.err
-make
+# cd ..
+# ./configure \
+#           --with-quickjs \
+#           --cc-opt="$CC_OPT -Iquickjs-ng" \
+#           --ld-opt="$LD_OPT -Lquickjs-ng/build" \
+# || cat build/autoconf.err
+# make
 
 
 # njs
-# git clone https://github.com/bellard/quickjs
-# cd quickjs
-# make libquickjs.a
-# cd ..
+git clone https://github.com/bellard/quickjs
+cd quickjs
+make libquickjs.a
+cd ..
 
-# ./configure \
-#           --with-quickjs \
-#           --cc-opt="$CC_OPT -Iquickjs" \
-#           --ld-opt="$LD_OPT -Lquickjs" \
-# || cat build/autoconf.err
-# make
+./configure \
+          --with-quickjs \
+          --cc-opt="$CC_OPT -Iquickjs" \
+          --ld-opt="$LD_OPT -Lquickjs" \
+|| cat build/autoconf.err
+make
 
 mkdir ../dist
 
