459367eb42e89b3cab4355e39956ce98cd76a2ae
ahaoboy <504595380@qq.com>
2025-06-19 17:22:49 +0800

    init

---
+++ build.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "not found target"
+    exit 1
+fi
+
+TARGET=$1
+
+git clone https://github.com/nginx/njs.git njs --depth=1
+cd njs
+
+# njs-ng
+# git clone https://github.com/quickjs-ng/quickjs quickjs-ng
+# cd quickjs-ng
+# git checkout v0.9.0
+# CFLAGS="$CC_OPT -fPIC" LDFLAGS=$LD_OPT cmake -B build
+# cmake --build build --target njs -j $(nproc)
+#
+# ./configure \
+#           --with-quickjs \
+#           --cc-opt="$CC_OPT -Iquickjs-ng" \
+#           --ld-opt="$LD_OPT -Lquickjs-ng/build" \
+# || cat build/autoconf.err
+# $MAKE_UTILITY -j$(nproc)
+
+
+# njs
+git clone https://github.com/bellard/quickjs
+cd quickjs
+# CFLAGS=$CC_OPT LDFLAGS=$LD_OPT $MAKE_UTILITY -j$(nproc) libquickjs.a
+make libquickjs.a
+cd ..
+
+./configure \
+          --with-quickjs \
+          --cc-opt="$CC_OPT -Iquickjs" \
+          --ld-opt="$LD_OPT -Lquickjs" \
+|| cat build/autoconf.err
+make
+
+mkdir ../dist
+
+cp ./build/njs ../dist
+cp ./build/libqjs* ../dist
+cp ./build/libnjs* ../dist
+
+cd ..
+
+ls -lh dist
+
+tar -czf ./njs-${TARGET}.tar.gz -C dist .
+ls -l ./njs-${TARGET}.tar.gz
